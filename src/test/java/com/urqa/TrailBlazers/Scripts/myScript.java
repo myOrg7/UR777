0c8198b62c124bdcfe1b89520737008d044746e5
utk01r <utk01r@gmail.com>
2018-11-20 23:32:01 +0530

    Starting the TB project Mob Auto

---
+++ src/test/java/com/urqa/TrailBlazers/Scripts/myScript.java
@@ -0,0 +1,33 @@
+package com.urqa.TrailBlazers.Scripts;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+
+public class myScript {
+	public static void main(String [] args){
+		AndroidDriver driver = null;
+		
+		DesiredCapabilities dc = new DesiredCapabilities();
+		dc.setCapability("automationName", "UiAutomator2");
+		dc.setCapability("platformName", "Android");
+		dc.setCapability("platformVersion", "7.0");
+		dc.setCapability("deviceName", "ZY223Q92ZC");
+		dc.setCapability("appPackage", "trailBlaze.com.trailBlaze");
+		dc.setCapability("appActivity", "trailBlaze.com.trailBlaze.Ui.Authentication.Activities.SplashActivity");
+		dc.setCapability("noReset", true);
+		
+		try {
+			driver = new AndroidDriver<MobileElement>(new URL("http://0.0.0.0:4723/wd/hub"), dc);
+		} catch (MalformedURLException e){
+			e.printStackTrace();
+		}
+		
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+	}
+}
